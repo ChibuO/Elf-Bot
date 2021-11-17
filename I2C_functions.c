5d7e079f9a7c4ba6195526d2220d60ccdc7ec132
cpurnama55 <cpurnama55@gmail.com>
2021-11-16 17:19:23 -0800

    Created seperate I2C functions file

---
+++ I2C_functions.c
@@ -0,0 +1,23 @@
+#include "I2C_functions.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+uint8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, const nrf_twi_mngr_t* i2c_manager){
+  uint8_t rx_buf = 0;
+  nrf_twi_mngr_transfer_t const read_transfer[] = {
+    NRF_TWI_MNGR_WRITE(i2c_addr, &reg_addr, 1, NRF_TWI_MNGR_NO_STOP),
+    NRF_TWI_MNGR_READ(i2c_addr, &rx_buf, 1, 0)
+  };
+  nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 2, NULL);
+
+  return rx_buf;
+}
+
+void i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t data, const nrf_twi_mngr_t* i2c_manager) {
+  //Note: there should only be a single two-byte transfer to be performed
+  uint8_t tx_buf[2] = { reg_addr, data };
+  nrf_twi_mngr_transfer_t const read_transfer[] = {
+    NRF_TWI_MNGR_WRITE(i2c_addr, tx_buf, 2, 0)
+  };
+  nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 1, NULL);
+}
---
+++ I2C_functions.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "nrf_twi_mngr.h"
+
+// Helper function to perform a 1-byte I2C read of a given register
+//
+// i2c_addr - address of the device to read from
+// reg_addr - address of the register within the device to read
+//
+// returns 8-bit read value
+uint8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, const nrf_twi_mngr_t* i2c_manager);
+
+// Helper function to perform a 1-byte I2C write of a given register
+//
+// i2c_addr - address of the device to write to
+// reg_addr - address of the register within the device to write
+void i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t data, const nrf_twi_mngr_t* i2c_manager);
