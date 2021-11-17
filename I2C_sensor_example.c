0d0174c8fe34ae93266f2839f9f812f102065f24
cpurnama55 <cpurnama55@gmail.com>
2021-11-16 16:01:46 -0800

    motor driver implemented

--- I2C_sensor_example.c
+++ I2C_sensor_example.c
@@ -10,6 +10,23 @@
 #include "nrf_delay.h"
 #include "math.h"
 
+  //left 8448 : 0x2100
+  //i2c address 89 : 0x59
+  //right: 8192 : 0x2000
+  //forward: 0: 0x8000
+  //reverse: 1 : 0x0000
+  //power on: 28673 : 0x70
+  //power off: 28672 
+  //left_invert: 0x13
+  //right_invert : 0x12
+#define mc_i2c_addr   0x59
+#define mc_left       0x21
+#define mc_right      0x20
+#define mc_fwd        0x80
+#define mc_rev        0x00
+#define mc_pwr_on     0x70
+#define mc_inv_left   0x13
+#define mc_inv_right  0x12
 
 // Pointer to an initialized I2C instance to use for transactions
 static const nrf_twi_mngr_t* i2c_manager = NULL;
@@ -36,43 +53,28 @@ static uint8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr) {
 // i2c_addr - address of the device to write to
 // reg_addr - address of the register within the device to write
 static void i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t data) {
-  //TODO: implement me
   //Note: there should only be a single two-byte transfer to be performed
   uint8_t tx_buf[2] = { reg_addr, data };
   nrf_twi_mngr_transfer_t const read_transfer[] = {
-    //TODO: implement me
     NRF_TWI_MNGR_WRITE(i2c_addr, tx_buf, 2, 0)
   };
   nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 1, NULL);
 }
 
-void invert() {
-  i2c_reg_write(0x59, 0x12, 1);
-}
 
-void down() {
-  //left 8448 : 0x2100
-  //i2c address 89 : 0x59
-  //right: 8192 : 0x2000
-  //forward: 0: 0x8000
-  //reverse: 1 : 0x0000
-  //power on: 28673 : 0x70
-  //power off: 28672 
-  //left_invert: 0x13
-  //right_invert : 0x12
-  //make left motor mowve forward
-  uint16_t pwr = (uint16_t)floor(30 * 127 / 100);
-  uint16_t pwr2 = (uint16_t)floor(29 * 127 / 100);
-  printf("pwr: %u\n", pwr);
-  uint16_t dir = pwr | 0x80;
-  uint16_t dir2 = pwr2 | 0x80;
-  uint16_t rev_dir = 127 - pwr;
-  printf("rev_dir: %u\n", rev_dir);
-  printf("dir: %u\n", dir);
-  i2c_reg_write(0x59, 0x21, dir); //make left motor move forward
-  printf("read: %u\n", i2c_reg_read(0x59, 0x21));
-  i2c_reg_write(0x59, 0x20, dir); //make right motor move forward
-  invert();
+void activate_servos(int l_speed, int r_speed, bool l_forward, bool r_forward) {
+  uint16_t l_pwr = (uint16_t)floor(l_speed * 127 / 100);
+  uint16_t r_pwr = (uint16_t)floor(r_speed * 127 / 100);
+  uint16_t l_dir = l_pwr | mc_fwd;
+  uint16_t r_dir = r_pwr | mc_fwd;
+  
+  i2c_reg_write(mc_i2c_addr, mc_inv_left, !l_forward); // left = 0 forwards, 1 backwards
+  i2c_reg_write(mc_i2c_addr, mc_inv_right, r_forward); // right = 0 backwards, 1 forwards
+
+  i2c_reg_write(mc_i2c_addr, mc_left, l_dir); //make left motor move forward
+  i2c_reg_write(mc_i2c_addr, mc_right,r_dir); //make right motor move forward
+  
+
 }
 
 // Initialize and configure the LSM303AGR accelerometer/magnetometer
@@ -82,8 +84,14 @@ void lsm303agr_init(const nrf_twi_mngr_t* i2c) {
   i2c_manager = i2c;
 
   i2c_reg_write(0x59, 0x70, 1); //power on motors
-  down();
-  nrf_delay_ms(5000);
+  activate_servos(30, 30, false, false);
+  nrf_delay_ms(500);
+  activate_servos(30, 30, true, true);
+  nrf_delay_ms(500);
+  activate_servos(30, 30, true, false);
+  nrf_delay_ms(500);
+  activate_servos(30, 30, false, true);
+  nrf_delay_ms(500);
   i2c_reg_write(0x59, 0x70, 0); //power off motors
 }
 
