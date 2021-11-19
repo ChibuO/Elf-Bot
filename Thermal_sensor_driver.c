5a6797ca49006aa83825b88fe649cd7ff2b37fba
ChibuO <chibuonyenemezu2023@u.northwestern.edu>
2021-11-19 12:12:17 -0800

    2d grid array

--- Thermal_sensor_driver.c
+++ Thermal_sensor_driver.c
@@ -7,8 +7,8 @@
 #include "math.h"
 
 // Use these values (in degrees C) to adjust the contrast
-#define HOT 40
-#define COLD 20
+#define HOT 30
+#define COLD 10
 
 // Pointer to an initialized I2C instance to use for transactions
 static const nrf_twi_mngr_t* i2c_manager = NULL;
@@ -55,9 +55,9 @@ float get_pixel_temp(uint8_t pixelAddr) {
     }
 
     //celsius
-    //float float_temp = (float)temp *.0625;
+    float float_temp = (float)temp *.25;
     //fahrenheit
-    float float_temp = ((float)temp *.0625) * 1.8 + 32;
+    //float float_temp = ((float)temp *.0625) * 1.8 + 32;
 
     return float_temp;
 
@@ -67,44 +67,64 @@ long map(long x, long in_min, long in_max, long out_min, long out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void grid_eye(uint8_t pixel_table[]) {
+void grid_eye(float pixel_table[8][8]) {
 
     // loop through all 64 pixels on the device and map each float value to a number
     // between 0 and 3 using the HOT and COLD values we set at the top of the sketch
-    for(uint8_t i = 0; i < 64; i++){
-        //map scales the given value
-        //value, min and max that we could get, min and max that we want
-        uint8_t orig_temp = get_pixel_temp(i);
-        pixel_table[i] = map(orig_temp, COLD, HOT, 0, 3);
-        //printf("orig: %u mapped: %u\n", orig_temp, pixel_table[i]);
+    for (uint8_t i = 0; i < 8; i++){
+        for(uint8_t j = 0; i < 8; i++){
+            //map scales the given value
+            //value, min and max that we could get, min and max that we want
+            uint8_t pixel_num = (i * 8) + j;
+            float orig_temp = get_pixel_temp(pixel_num);
+
+            /*if ( orig_temp < COLD ) {
+                orig_temp = COLD;
+            } else if ( orig_temp > HOT ) {
+                orig_temp = HOT;
+            }*/
+
+            pixel_table[i][j] = orig_temp;
+            printf("i: %u j: %u orig: %f\n", i, j, orig_temp);
+            //pixel_table[j] = map(orig_temp, COLD, HOT, 0, 5);
+            //    printf("i: %u orig: %f mapped: %f\n", pixel_num, orig_temp, pixel_table[pixel_num]);
+        }
     }
 
-    //printf("\n");
+    printf("\n");
 
     // loop through the table of mapped values and print a character corresponding to each
     // pixel's temperature. Add a space between each. Start a new line every 8 in order to 
     // create an 8x8 grid
-    for(uint8_t i = 0; i < 64; i++) {
-        if (pixel_table[i] == 0) {printf(".");}
-        else if (pixel_table[i] == 1) {printf("o");}
-        else if (pixel_table[i] == 2) {printf("0");}
-        else if (pixel_table[i] == 3) {printf("O");}
-        printf(" ");
+    for (uint8_t j = 0; j < 8; j++) {
+        for(uint8_t i = 0; i < 8; i++) {
+            printf("orig: %f\n", pixel_table[j][i]);
+            /*
+            uint8_t pixel_num = (j * 8) + i;
+            if (pixel_table[pixel_num] == 0) {printf("0");}
+            else if (pixel_table[pixel_num] == 1) {printf("1");}
+            else if (pixel_table[pixel_num] == 2) {printf("2");}
+            else if (pixel_table[pixel_num] == 3) {printf("3");}
+            else if (pixel_table[pixel_num] == 4) {printf("4");}
+            else if (pixel_table[pixel_num] == 5) {printf("5");}
+            printf(" ");
 
-        if ((i+1) % 8 == 0) {
-            printf("\n");
+            */
+            if ((i+1) % 8 == 0) {
+                printf("\n");
+            }
         }
     }
 }
 
 void thermal_init(const nrf_twi_mngr_t* i2c){
     i2c_manager = i2c;
-    uint8_t pixel_table[64];
+    float pixel_table[8][8];
     while(1){
         //printf("Temperature: %f\n", getDeviceTemp());
         grid_eye(pixel_table);
         printf("\n");
-        nrf_delay_ms(1000);
+        nrf_delay_ms(2000);
     }
 }
 
