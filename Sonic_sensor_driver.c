3de40fe880d205a2dbb94ec582082039404b37af
cpurnama55 <cpurnama55@gmail.com>
2021-11-20 16:00:13 -0800

    Logged possible hits for ultra sonic sensor address '

--- Sonic_sensor_driver.c
+++ Sonic_sensor_driver.c
@@ -3,21 +3,27 @@
 #include "nrf_delay.h"
 
 
-#define sonic_addr     0x00
+#define sonic_addr     2
 #define sonic_reg      0x57
-#define trig           0x5006
-#define echo           0x5008
+#define trig           0
+#define echo           0
 /*
 #define sonic_data_reg 0x005216
 #define sonic_status_reg 0x005217
 #define beep_reg   0x0050F0
 */
 
+// Log of possible hits:
+// - sonic_addr = 0; trig = 0; echo = 1
+// - sonic_addr = 2; trig = 0; echo = 0
+
 // Pointer to an initialized I2C instance to use for transactions
 static const nrf_twi_mngr_t* i2c_manager = NULL;
 
 
 void print_data(){
+    
+    
     for (int i = 0; i <= 255; i++)
     {
         printf("i: %u\n", i);
@@ -26,11 +32,11 @@ void print_data(){
             for (int k = 0; k<= 255; k++)
             {
                 i2c_reg_write(i, j, 1, i2c_manager);
-                nrf_delay_ms(6);
+                //nrf_delay_ms(6);
                 uint8_t data = i2c_reg_read(i, k, i2c_manager);
                 if (data != 0)
                 {
-                    printf("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\n");
+                    printf("hit\n");
                    // printf("i: %u\n", i);
                     printf("j: %u\n", j);
                     printf("k: %u\n", k);
@@ -40,9 +46,11 @@ void print_data(){
         }
     }
     printf("Function done\n");
+    
+    
     /*
     i2c_reg_write(sonic_addr, trig, 1, i2c_manager);
-    
+    nrf_delay_ms(1000);
     uint8_t distance_msb = i2c_reg_read(sonic_addr, echo, i2c_manager);
     uint8_t distance_lsb = i2c_reg_read(sonic_addr, echo+1, i2c_manager);
     printf("Dist_MSB: %u\n", distance_msb);
@@ -52,6 +60,7 @@ void print_data(){
     
     printf("sonic data: %u\n", distance);
     */
+    
 }
 
 /*  
@@ -63,7 +72,7 @@ void beep(){
 */
 
 void sonic_init(const nrf_twi_mngr_t* i2c){
-    /*
+    
     while(1){
         i2c_manager = i2c;
         printf("\n");
@@ -71,8 +80,9 @@ void sonic_init(const nrf_twi_mngr_t* i2c){
         print_data();
         nrf_delay_ms(2000);
     } 
-    */
+    /*
     i2c_manager = i2c;
     print_data();
+    */
     //beep();
 }
